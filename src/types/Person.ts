9384f4fa41df5cca65c34fd95bfae4d1319c3264
Vamsi Ampolu <vamsiampolu3000@gmail.com>
2025-04-08 11:26:34 +1000

    feat: Use quicktype to create JSON Schema from Typescript

---
+++ src/types/Person.ts
@@ -0,0 +1,20 @@
+/**
+ * Person base class
+ */
+/**
+ *
+ * @export
+ * @interface Person 
+*/
+export interface Person {
+  /**
+   * @type {string}
+   * @memberof Person
+  */
+  name: string
+  email: string;
+  phone?: string;
+  mobile: string;
+  address?: string;
+}
+
